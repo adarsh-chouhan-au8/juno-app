8b723b5239ff589a4ddf6b048d07856c9c98b548
adarsh-chouhan-au8 <adarsh.chouhan11@gmail.com>
2022-10-27 13:31:54 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import AllRoutes from './AllRoutes';
 import ScrollToTop from './components/ScrollToTop';
 
 
 function App() {
   return (
-   <Router>
-    <ScrollToTop/>
-    <AllRoutes />
+    <Router>
+      <ScrollToTop />
+      <AllRoutes />
     </Router>
   );
 }
